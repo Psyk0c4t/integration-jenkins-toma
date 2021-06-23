1e587b79475a447a525c20fe99cb2f33fa4fb559
Toma-png <thomas.debail@epsi.fr>
2021-06-23 11:33:13 +0200

    add-all

--- src/main/java/fr/epsi/junit/Calculator.java
+++ src/main/java/fr/epsi/junit/Calculator.java
@@ -2,23 +2,32 @@ package fr.epsi.junit;
 
 public class Calculator {
 
-    public int add(int a, int b){
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public int multi(int a, int b){
+    public int multi(int a, int b) {
         return a * b;
     }
 
-    public int substract(int a, int b){
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
         return a / b;
     }
 
-    public int div(int a, int b){
-        return a/b;
-    }
-
-    public int add(int[] nmbers){
-        return 0;
+    public int add(int[] nmbers) {
+        int[] tableau = nmbers;
+        for (int i = 0; i < tableau.length; i++) {
+            tableau[i] = i - 5;
+        }
+        int somme = 1+2+3+4+5;
+        for (int val : tableau) {
+            somme += val;
+            return somme;
+        }
+        return somme;
     }
 }
--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -1,23 +1,58 @@
 package fr.epsi.junit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class CalculatorTest {
 
     private Calculator calculator;
 
+    @BeforeEach
     public void init(){
-        //TODO
+        System.out.println("---------------------------");
+        System.out.println("Init globale");
+        this.calculator = new Calculator();
     }
-
+    @Test
     public void should_add_two_numbers(){
-       // TODO
+        int a = 3;
+        int b = 6;
+        int result = a + b;
+        Assertions.assertEquals(result, this.calculator.add(a, b));
+        System.out.println("Addition terminé");
     }
-
+    @Test
     public void should_substract_two_numbers(){
-        // TODO
+        int a = 3;
+        int b = 6;
+        int result  = a - b;
+        Assertions.assertEquals(result, this.calculator.substract(a, b));
+        System.out.println("Soustraction terminé");
     }
-
-
-}
+    @Test
+    public void should_multi_two_numbers(){
+        int a = 3;
+        int b = 6;
+        int result = a*b;
+        Assertions.assertEquals(result, this.calculator.multi(a, b));
+        System.out.println("Multiplication terminé");
+    }
+    @Test
+    public void should_div_two_numbers(){
+        int a = 3;
+        int b = 6;
+        int result = 3/6;
+        Assertions.assertEquals(result, this.calculator.div(a, b));
+        System.out.println("Division terminé");
+    }
+    @Test
+    public void add_tableau(){
+        int[] nmbers = {1,2,3,4,5};
+            Assertions.assertEquals(10, this.calculator.add(nmbers));
+            System.out.println("Tableau terminé");
+        }
+    }
